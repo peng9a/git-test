f5d54f7af0765e176bad29f27bda13c18cb44cb6
PengChangShun <1276179285@qq.com>
2022-03-14 16:57:10 +0800

    second commit

---
+++ src/main/java/com/peng/GitTest2.java
@@ -0,0 +1,12 @@
+package com.peng;
+
+/**
+ * @author 12761
+ * @create 2022/3/14 16:52
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
