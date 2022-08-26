c42aa6ace38a0a3b0253d2b0ae2f3bfdd12f1e5e
pengchangshun <1276179285@qq.com>
2022-08-26 21:42:08 +0800

    first commit

---
+++ src/main/java/com/peng/GitTest.java
@@ -0,0 +1,12 @@
+package com.peng;
+
+/**
+ * @author 12761
+ * @create 2022-08-26 19:00
+ * @description 描述
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
