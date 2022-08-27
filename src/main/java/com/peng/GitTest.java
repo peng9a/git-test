20bf2690a485df56bf39a934f6e0d376c333642c
pengchangshun <1276179285@qq.com>
2022-08-27 11:20:18 +0800

    push test commit1

--- src/main/java/com/peng/GitTest.java
+++ src/main/java/com/peng/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("version 4");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test");
     }
 }
